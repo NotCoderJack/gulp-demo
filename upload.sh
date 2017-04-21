30787997c2bf434b7f9d5f4c0db51a150e65abe5
haojie <haojie@taotaosou.com>
2017-04-21 16:06:06 +0800

    upload -deploy

--- upload.sh
+++ upload.sh
@@ -25,7 +25,7 @@ fi
 
 # 上传测试FTP
 if [ "$1" == "-test" ]; then
-    echo 'Note: before your upload, check the env file again! Sure to upload ? yes/no:'
+    echo 'Note: before your upload, check the env file again! Sure to upload your project for TEST? yes/no:'
     read confirm
     if [ "$confirm" == "yes" ]; then
         git pull origin "$master"
@@ -34,29 +34,37 @@ if [ "$1" == "-test" ]; then
         gulp lines
         echo -e "\n\033[92m Upload to test ftp successfully.\n"
     else
-        echo "Upload Canceled!"
+        echo "Cancel Test Successfully!"
         exit
     fi
 
 #上传发布ftp
 elif [ "$1" == "-depoly" ]; then
-    git pull origin "$master"
-    git pull origin "$branch"
-    git push origin "$branch"
+    echo 'Note: before your upload, check the env file again! Sure to upload your project for TEST? yes/no:'
+    read confirm
+    if [ "$confirm" == "yes" ]; then
+        git pull origin "$master"
+        git pull origin "$branch"
+        git push origin "$branch"
 
-    git checkout "$master"
-    git merge "$branch"
-    git push origin "$master"
+        git checkout "$master"
+        git merge "$branch"
+        git push origin "$master"
 
-    gulp deploy
+        gulp deploy
 
-    git add .
-    git commit -m 'new release'
-    git push origin master
-    git checkout "$branch"
-    git pull origin master
-    git push origin "$branch"
-    echo -e "\n\033[92m Upload to deploy ftp successfully.\n"
+        git add .
+        git commit -m 'new release'
+        git push origin master
+        git checkout "$branch"
+        git pull origin master
+        git push origin "$branch"
+        echo -e "\n\033[92m Upload to deploy ftp successfully.\n"
+    else
+        echo "Cancel Deploy Successfully !"
+        exit
+    fi
+
 
 else
   echo -e "\033[31m Please select upload mode. -test or -deploy"
